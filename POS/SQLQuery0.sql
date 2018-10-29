b39fa159af307f52b8efab7d9a4faf528f1b43c8
Dodam Shin <dshin20@my.centennialcollege.ca>
2018-10-28 20:23:11 -0400

    Delete SQLQuery0.sql

--- POS/SQLQuery0.sql
+++
@@ -1,6 +0,0 @@
-select * from tblItem;
-
-
-INSERT INTO tblItem(itemName, itemImage)  
-select 'testimage',BulkColumn
-FROM OPENROWSET(Bulk 'C:\Users\susur\Dropbox\document\test.jpg', SINGLE_BLOB) AS img;
